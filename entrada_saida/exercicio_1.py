50b955781e1cccf2fde14061f05c90aa65266109
EvelynBotta <evelyn.ferr.b@gmail.com>
2024-05-07 09:37:27 -0300

    exercicio 1

---
+++ entrada_saida/exercicio_1.py
@@ -0,0 +1,6 @@
+numero_1=int(input('Insira o primeiro número(inteiro):'))
+numero_2=int(input('Insira o segundo número(inteiro):'))
+soma=numero_1+numero_2
+print(f'O resultado da soma dos dois números é: {soma}')
+produto=numero_1*numero_2
+print(f'O produto entre eles é: {produto}')
