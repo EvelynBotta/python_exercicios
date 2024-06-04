9502a05c42f73ff5b647f410828356723a427ada
EvelynBotta <evelyn.ferr.b@gmail.com>
2024-06-04 12:09:26 -0300

    exercicio3.py

---
+++ funcoes/exercicio_3.py
@@ -0,0 +1,15 @@
+def calcular_media(n1, n2, n3, n4):
+  media = (n1+n2+n3+n4)/4
+  return media 
+  
+def status_aluno(m):
+    if m >= 5:
+      return 'Aprovado'
+    else:
+        return 'Reprovado'
+
+print('Calculadora de Média')
+print('Digite 4 notas: ')
+numeros = []
+numeros.append()
+
